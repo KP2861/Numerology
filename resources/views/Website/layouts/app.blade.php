9228ece1507af757b49901c63303d335259d3364
tanisha <tanisha@seasia.in>
2024-09-02 12:28:51 +0530

    home-page-icons

--- resources/views/Website/layouts/app.blade.php
+++ resources/views/Website/layouts/app.blade.php
@@ -17,6 +17,10 @@
     crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
     <!--srart theme style -->
     <link rel="stylesheet" type="text/css" href="{{ url('frontend/scss/animate.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{ url('frontend/scss/bootstrap.css') }}" />
